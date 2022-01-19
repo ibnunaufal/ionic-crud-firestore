3f4def69622012f7cb7e3491494fa3bbd7610e14
Ibnu Naufal <ibnunaufal5758@gmail.com>
2022-01-19 18:38:32 +0700

    initial commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyBeO1RbpOTfK7Dti4l98vcfXMEmBz8Lwi0",
+    authDomain: "simple-crud-efb73.firebaseapp.com",
+    projectId: "simple-crud-efb73",
+    storageBucket: "simple-crud-efb73.appspot.com",
+    messagingSenderId: "662967896890",
+    appId: "1:662967896890:web:d003328fd8db623a231e03"
+  }
+};
+
